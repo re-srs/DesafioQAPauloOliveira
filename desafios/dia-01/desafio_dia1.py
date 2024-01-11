1092284ee2d7c82fbdc8f85a6dbd8028e993d2e4
Renata <re_srs@yahoo.com.br>
2024-01-11 15:10:01 -0300

    Desafio 01

---
+++ desafios/dia-01/desafio_dia1.py
@@ -0,0 +1,8 @@
+numero_digitado = int(input("Digite um numero: "))
+resultado = numero_digitado % 2
+
+if numero_digitado % 2 == 0:
+        print("O numero {} eh PAR!!!".format(numero_digitado))
+else:
+        print("O numero {} eh IMPAR!!!".format(numero_digitado))
+    
